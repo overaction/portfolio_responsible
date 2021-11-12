a19690a035c94c3c21f08b304fe63cd47096f2d2
mckim <kmc04@naver.com>
2021-11-13 01:35:11 +0900

    Navbar 생성 + Navbarcss

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,7 +1,26 @@
+import styles from '../styles/Navbar.module.css'
+import Link from 'next/link';
 const Navbar = () => {
     return (
-        <div>
-            navbar
+        <div className={styles.container}>
+            <Link href="/">avocado</Link>
+            <ul className={styles.list}>
+                <li className={styles.listItem}>
+                    <Link href="/products/design">디자인</Link>
+                </li>
+                <li className={styles.listItem}>
+                    <Link href="/products/development">개발</Link>
+                </li>
+                <li className={styles.listItem}>
+                    <Link href="/products/product">제품</Link>
+                </li>
+                <li className={styles.listItem}>
+                    <Link href="/products/photography">포토그래피</Link>
+                </li>
+                <li className={styles.listItem}>
+                    <Link href="/products/contact">컨택</Link>
+                </li>
+            </ul>
         </div>
     )
 }
