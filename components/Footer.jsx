76c7b272397e7b77e0636cefac5a9864121728a8
mckim <kmc04@naver.com>
2021-11-14 00:46:54 +0900

    footer 제작

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,8 +1,32 @@
+import styles from "../styles/Footer.module.css"
+import Circle from "./Circle"
+import Image from "next/image";
+import Link from "next/link"
 
 const Footer = () => {
     return (
-        <div>
-            Footer
+        <div className={styles.container}>
+            <div className={styles.cardL}>
+                <h1 className={styles.title}>제목</h1>
+                <h1 className={styles.linkTitle}>
+                    <a href="/contact" className={styles.link}>
+                        <span className={styles.linkText}>링크</span>
+                        <Image className={styles.linkImg} src="/img/link.png" width="40px" height="40px" />
+                    </a>
+                </h1>
+            </div>
+            <div className={styles.cardS}>
+                <div className={styles.cardItem}>내용</div>
+                <div className={styles.cardItem}>
+                    contact me <br /> 010 1234 1234
+                </div>
+            </div>
+            <div className={styles.cardS}>
+                <div className={styles.cardItem}>내용</div>
+                <div className={styles.cardItem}>
+                    contact me <br /> 010 1234 1234
+                </div>
+            </div>
         </div>
     )
 }
