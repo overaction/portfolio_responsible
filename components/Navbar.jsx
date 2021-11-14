506d86eacc4204e007df6a0c75053ee2ade3abc9
mckim <kmc04@naver.com>
2021-11-15 02:36:14 +0900

    모바일 메뉴바

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,6 +1,8 @@
 import styles from '../styles/Navbar.module.css'
 import Link from 'next/link';
+import { useState } from 'react';
 const Navbar = () => {
+    const [open,setOpen] = useState(false);
     return (
         <div className={styles.container}>
             <Link href="/">avocado</Link>
@@ -18,6 +20,28 @@ const Navbar = () => {
                     <Link href="/products/photography">포토그래피</Link>
                 </li>
                 <li className={styles.listItem}>
+                    <Link href="/contact">컨택</Link>
+                </li>
+            </ul>
+            <div className={styles.hamburger} onClick={e => setOpen(!open)}>
+                <div className={styles.line}></div>
+                <div className={styles.line}></div>
+                <div className={styles.line}></div>
+            </div>
+            <ul onClick={e => setOpen(false)} className={styles.menu} style={{right: open ? "0" : "-50vw"}}>
+                <li className={styles.menuItem}>
+                    <Link href="/products/design">디자인</Link>
+                </li>
+                <li className={styles.menuItem}>
+                    <Link href="/products/development">개발</Link>
+                </li>
+                <li className={styles.menuItem}>
+                    <Link href="/products/production">제품</Link>
+                </li>
+                <li className={styles.menuItem}>
+                    <Link href="/products/photography">포토그래피</Link>
+                </li>
+                <li className={styles.menuItem}>
                     <Link href="/contact">컨택</Link>
                 </li>
             </ul>
